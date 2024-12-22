af07fe4c2f7cf0e43ed1ad278e55c319590022cb
abhishekpawardev <abhipawarraipur@gmail.com>
2024-12-22 20:08:05 +0530

    8:00

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,9 +2,26 @@ import React from "react";
 
 const App = () =>{
   return(
-    <div>
-      <h1>Hello World !</h1>
-    </div>
+    <>
+  <NetflixPage/>
+  <NetflixPage/>
+  <NetflixPage/>
+  <NetflixPage/>
+  <NetflixPage/>
+  <NetflixPage/>
+    </>
+  )
+}
+
+const NetflixPage = () => {
+  return(
+    <>
+    <h2>Film Name</h2>
+        <img src="../src/assets/poster1.jpg" alt="xd"  height='500px'/>
+        <h3>IMDB Ratting 8.3</h3>
+        <p>Title : Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quaerat ab ipsa labore eaque corrupti, ut adipisci, delectus temporibus amet, recusandae illo voluptatem fugit ad nam. Fuga numquam quo eius.</p>
+    
+    </>
   )
 }
 
