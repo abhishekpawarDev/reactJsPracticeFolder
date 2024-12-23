70b114a744739ba3488281346e5ef3708d86211e
abhishekpawardev <abhipawarraipur@gmail.com>
2024-12-23 10:01:12 +0530

    10:00 AM #12

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import NetflixPage from "./components/netflixCard";
 const App = () =>{
   return(
     <>
@@ -13,16 +13,6 @@ const App = () =>{
   )
 }
 
-const NetflixPage = () => {
-  return(
-    <>
-    <h2>Film Name</h2>
-        <img src="../src/assets/poster1.jpg" alt="xd"  height='500px'/>
-        <h3>IMDB Ratting 8.3</h3>
-        <p>Title : Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quaerat ab ipsa labore eaque corrupti, ut adipisci, delectus temporibus amet, recusandae illo voluptatem fugit ad nam. Fuga numquam quo eius.</p>
-    
-    </>
-  )
-}
+
 
 export default App;
---
+++ react-practice/src/components/netflixCard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NetflixPage = () => {
+    return(
+      <>
+      <h2>Film Name</h2>
+          <img src="../src/assets/poster1.jpg" alt="xd"  height='500px'/>
+          <h3>IMDB Ratting 8.3</h3>
+          <p>Title : Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quaerat ab ipsa labore eaque corrupti, ut adipisci, delectus temporibus amet, recusandae illo voluptatem fugit ad nam. Fuga numquam quo eius.</p>
+      
+      </>
+    )
+  }
+
+export default NetflixPage;
